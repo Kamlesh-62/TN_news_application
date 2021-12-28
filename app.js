5d130099b41793a2025ab3caac3f907061dd9cb5
Kamlesh Prajapati <kamleshprajapati@Kamleshs-MacBook-Air.local>
2021-12-27 20:15:58 -0500

    More update, make search input more functional and left and right button make it functional to scroll left and right, need to organize code

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
 // global object
 const newsApp = {}; 
+
 newsApp.init = () =>{
-
-}
-
-newsApp.url = 'https://api.currentsapi.services/v1/';
-
-newsApp.userSearchGetNews = (userInput) =>{
-
-const url = new URL('https://api.currentsapi.services/v1/search');
-
-url.search = new URLSearchParams({
-    apiKey: 'Kt9jlerS23m4XmNMBGn8BajZA7NdUlj6qsEjoyPfhzsBbQ6T',
-    language: "en",
-    keywords: userInput,
-});
-
-fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then((jsonResult) => {
-        // console.log(jsonResult.news);
-        newsApp.filterListOfArray(jsonResult.news)
-        newsApp.printUserSearchData(newsApp.filteredData);
-    });
+    newsApp.userSearchGetNews("Canada");
+    newsApp.getUserSearch();
+    newsApp.scrollNewsSection();
 }
 
 // create empty array, to fill data with image only and ignore data without image
@@ -35,46 +15,86 @@ newsApp.filterListOfArray = (arryData) => {
         return listOfArray = listOfArray.image !== "None";
     });
 }
+
+// left and right button functionality
+newsApp.scrollNewsSection = () => {
+    newsApp.leftBtn = document.querySelector('.left-btn');
+    newsApp.rightBtn = document.querySelector('.right-btn');
+    newsApp.newsWindow = document.querySelector('#user-search-news');
+
+    newsApp.leftBtn.addEventListener('click', () => {
+        newsApp.newsWindow.scrollLeft -= 250;
+    });
+
+    newsApp.rightBtn.addEventListener('click', () => {
+        newsApp.newsWindow.scrollLeft += 250;
+    })
+}
+
+
+newsApp.url = 'https://api.currentsapi.services/v1/';
+
+newsApp.userSearchGetNews = (userInput) =>{
+
+const url = new URL('https://api.currentsapi.services/v1/search');
+
+url.search = new URLSearchParams({
+    apiKey: 'Kt9jlerS23m4XmNMBGn8BajZA7NdUlj6qsEjoyPfhzsBbQ6T',
+    language: "en",
+    keywords: userInput
+});
+
+fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((jsonResult) => {
+        newsApp.filterListOfArray(jsonResult.news)
+        newsApp.printUserSearchData(newsApp.filteredData);
+    });
+}
+
 // create function which will print data on website......
 newsApp.printUserSearchData = (arryData) =>{
-    const ulElement = document.querySelector('.listOfUserSearchNews');
-    ulElement.innerHTML = "";
+    newsApp.ulElement = document.querySelector('.listOfUserSearchNews');
+    newsApp.ulElement.innerHTML = "";
     arryData.forEach((listOfArray) => {
         
-        const listElement = document.createElement('li');
-        const headerElement = document.createElement('h3');
-        const imgElement = document.createElement('img');
-        const paragraphElement = document.createElement('p');
-        const divElement = document.createElement('div');
-        const anchorElement = document.createElement('a');
+        newsApp.listElement = document.createElement('li');
+        newsApp.headerElement = document.createElement('h3');
+        newsApp.imgElement = document.createElement('img');
+        newsApp.paragraphElement = document.createElement('p');
+        newsApp.divElement = document.createElement('div');
+        newsApp.anchorElement = document.createElement('a');
         
-        headerElement.innerHTML = listOfArray.title;
-        imgElement.src = listOfArray.image;
-        imgElement.alt = listOfArray.title;
-        paragraphElement.innerHTML = listOfArray.description;
-        anchorElement.href = listOfArray.url;
-        anchorElement.innerHTML = `Read More`;
-        anchorElement.target = '_blank'; 
-
-
-        ulElement.appendChild(listElement);
-        divElement.appendChild(anchorElement);
-        listElement.append(headerElement, imgElement, paragraphElement,divElement); 
-
+        newsApp.headerElement.innerHTML = listOfArray.title;
+        newsApp.imgElement.src = listOfArray.image;
+        newsApp.imgElement.alt = listOfArray.title;
+        newsApp.paragraphElement.innerHTML = listOfArray.description;
+        newsApp.anchorElement.href = listOfArray.url;
+        newsApp.anchorElement.innerHTML = `Read More`;
+        newsApp.anchorElement.target = '_blank'; 
+        
+        
+        
+        newsApp.ulElement.appendChild(newsApp.listElement);
+        newsApp.divElement.appendChild(newsApp.anchorElement);
+        newsApp.listElement.append(newsApp.headerElement, newsApp.imgElement, newsApp.paragraphElement, newsApp.divElement); 
+        
     })
 } 
-newsApp.userSearchGetNews("world");
+
+
 
 // Event listener for user search input and get data accordingly user's choice....
 newsApp.getUserSearch = () => {
     const form = document.querySelector('#home-form');
-    const searchInput = document.querySelector('.search-input').value;
-    searchInput.value = "";
-    
+    const searchOutPutValue = document.querySelector('.userSearch-outPut-Value');
     form.addEventListener('submit', (event) => {
-        event.preventDefault();
+        const searchInput = document.querySelector('.search-input').value;
         newsApp.userSearchGetNews( searchInput);
+        searchOutPutValue.innerHTML = searchInput;
+        event.preventDefault();
     })    
 }
-newsApp.getUserSearch();
 newsApp.init();
